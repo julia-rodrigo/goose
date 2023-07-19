b2d380d6307911812113f262f5eec725632e23a2
julia-rodrigo <juliarodrigo2301@gmail.com>
2023-07-19 19:37:41 +0100

    new: added shadcn and some routes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>
+        THis is tendo maya 
+      </p>
+
+      <p>
+        Dashboard Page (Protected)
+      </p>
+    </div>
+  )
+}
+
+export default DashboardPage;
