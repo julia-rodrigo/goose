d976a9f2bff971a06f253dacf2f22b3344602371
julia-rodrigo <juliarodrigo2301@gmail.com>
2023-07-20 01:43:37 +0100

    UI: dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,15 +1,86 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+'use client';
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { MessageSquare, ArrowRight, Music, ImageIcon, Code, VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation"
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music"
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image"
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video"
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code"
+  },
+]
+
 
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
-      <p>
-        THis is tendo maya 
-      </p>
-      <p>
-        Dashboard Page (Protected)
-      </p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI 
+          <span className="text-blue-500 hover:text-teal-400 duration-300"> @Goosle</span>
+        </h2>
+        <div className="px-6">
+          <p className="text-muted-foreground font-light hover:font-medium duration-500 text-sm md:text-lg text-center ">
+            Chat with the leading top star AI - Experience a legend AI
+          </p>
+        </div>
+        <p>
+          THis is tendo maya. GoosleMe
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+          onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">
+                {tool.label}
+              </div>
+            </div>
+            <ArrowRight className="w-5 h-5"/>
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }
